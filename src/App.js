8024d0b598013d414bdcff234f9a27d7e5512d9d
Santiago Nardini <nardinisantiago@gmail.com>
2023-04-20 18:30:26 -0300

    Context

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Nosotros from './components/Nosotros/Nosotros';
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer';
+import { CartProvider } from './context/CartContext';
+import Cart from './components/Cart/Cart';
 
 function App() {
 
   return (
+<CartProvider>
+      
     <BrowserRouter>
 
       <Navbar />
@@ -16,6 +20,7 @@ function App() {
         <Route path='/' element={ <ItemListContainer /> }/>
         <Route path='/productos/:categoryId' element={ <ItemListContainer /> }/>
         <Route path='/detail/:itemId' element={ <ItemDetailContainer /> } />
+        <Route path='/cart' element={ <Cart /> } />
         <Route path='/nosotros' element={ <Nosotros /> }/>
         <Route path='*' element={ <Navigate to={"/"}/> }/>
       </Routes>
@@ -23,6 +28,8 @@ function App() {
       {/* <Footer /> */}
 
     </BrowserRouter>
+    </CartProvider>
+
   );
 }
 
